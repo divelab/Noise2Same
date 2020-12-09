4bbb0ee309769e873fee0fc2a158e221003e6562
Abubakar Abid <a12d@stanford.edu>
2020-12-09 13:05:02 -0600

    Update demo.py

--- demo.py
+++ demo.py
@@ -5,12 +5,12 @@ from models import Noise2Same
 
 os.system("mkdir trained_models/denoising_ImageNet")
 os.system("cd trained_models/denoising_ImageNet")
-os.system("gdown 1asrwULW1lDFasystBc3UfShh5EeTHpkW")
-os.system("gdown 1Re1ER7KtujBunN0-74QmYrrOx77WpVXK")
-os.system("gdown 1QdlyUPUKyyGtqD0zBrj5F7qQZtmUELSu")
-os.system("gdown 1LQsYR26ldHebcdQtP2zt4Mh-ZH9vXQ2S")
-os.system("gdown 1AxTDD4dS0DtzmBywjGyeJYgDrw-XjYbc")
-os.system("gdown 1w4UdNAbOjvWSL0Jgbq8_hCniaxqsbLaQ")
+os.system("gdown https://drive.google.com/uc?id=1asrwULW1lDFasystBc3UfShh5EeTHpkW")
+os.system("gdown https://drive.google.com/uc?id=1Re1ER7KtujBunN0-74QmYrrOx77WpVXK")
+os.system("gdown https://drive.google.com/uc?id=1QdlyUPUKyyGtqD0zBrj5F7qQZtmUELSu")
+os.system("gdown https://drive.google.com/uc?id=1LQsYR26ldHebcdQtP2zt4Mh-ZH9vXQ2S")
+os.system("gdown https://drive.google.com/uc?id=1AxTDD4dS0DtzmBywjGyeJYgDrw-XjYbc")
+os.system("gdown https://drive.google.com/uc?id=1w4UdNAbOjvWSL0Jgbq8_hCniaxqsbLaQ")
 os.system("cd ../..")
 
 model = Noise2Same('trained_models/', 'denoising_ImageNet', dim=2, in_channels=3)
