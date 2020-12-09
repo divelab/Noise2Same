bda117b10cd6e6b0ab0086d4ced4b34d426f4b53
Abubakar Abid <a12d@stanford.edu>
2020-12-09 15:05:22 -0600

    Update demo.py

--- demo.py
+++ demo.py
@@ -5,10 +5,7 @@ from models import Noise2Same
 import gradio as gr
 
 os.system("mkdir trained_models/denoising_ImageNet")
-os.system("cd trained_models/denoising_ImageNet")
-os.system("gdown https://drive.google.com/uc?id=1asrwULW1lDFasystBc3UfShh5EeTHpkW")
-os.system("gdown https://drive.google.com/uc?id=1Re1ER7KtujBunN0-74QmYrrOx77WpVXK")
-os.system("gdown https://drive.google.com/uc?id=1QdlyUPUKyyGtqD0zBrj5F7qQZtmUELSu")
+os.system("cd trained_models/denoising_ImageNet; gdown https://drive.google.com/uc?id=1asrwULW1lDFasystBc3UfShh5EeTHpkW; gdown https://drive.google.com/uc?id=1Re1ER7KtujBunN0-74QmYrrOx77WpVXK; gdown https://drive.google.com/uc?id=1QdlyUPUKyyGtqD0zBrj5F7qQZtmUELSu")
 os.system("gdown https://drive.google.com/uc?id=1LQsYR26ldHebcdQtP2zt4Mh-ZH9vXQ2S")
 os.system("gdown https://drive.google.com/uc?id=1AxTDD4dS0DtzmBywjGyeJYgDrw-XjYbc")
 os.system("gdown https://drive.google.com/uc?id=1w4UdNAbOjvWSL0Jgbq8_hCniaxqsbLaQ")
