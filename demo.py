307a0e58673dcca8c1390673887099fc510b7ef9
Abubakar Abid <a12d@stanford.edu>
2020-12-09 15:28:53 -0600

    Update demo.py

--- demo.py
+++ demo.py
@@ -6,9 +6,9 @@ import gradio as gr
 
 os.system("mkdir trained_models/denoising_ImageNet")
 os.system("cd trained_models/denoising_ImageNet; gdown https://drive.google.com/uc?id=1asrwULW1lDFasystBc3UfShh5EeTHpkW; gdown https://drive.google.com/uc?id=1Re1ER7KtujBunN0-74QmYrrOx77WpVXK; gdown https://drive.google.com/uc?id=1QdlyUPUKyyGtqD0zBrj5F7qQZtmUELSu; gdown https://drive.google.com/uc?id=1LQsYR26ldHebcdQtP2zt4Mh-ZH9vXQ2S; gdown https://drive.google.com/uc?id=1AxTDD4dS0DtzmBywjGyeJYgDrw-XjYbc; gdown https://drive.google.com/uc?id=1w4UdNAbOjvWSL0Jgbq8_hCniaxqsbLaQ; cd ../..")
-os.system("!wget -O arch.png https://i.imgur.com/NruRABn.png")
-os.system("!wget -O parrot.png https://i.imgur.com/zdji3xv.png")
-os.system("!wget -O lion.png https://i.imgur.com/qNT0lJJ.png")
+os.system("wget -O arch.png https://i.imgur.com/NruRABn.png")
+os.system("wget -O parrot.png https://i.imgur.com/zdji3xv.png")
+os.system("wget -O lion.png https://i.imgur.com/qNT0lJJ.png")
 
 model = Noise2Same('trained_models/', 'denoising_ImageNet', dim=2, in_channels=3)
 
